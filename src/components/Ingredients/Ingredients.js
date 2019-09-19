d358b72480bdc22b9cb09b9f986699da48b2da2e
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-09-19 13:12:36 +0300

    styling a bit

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,35 +1,35 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 
 const Ingredients = ()  => {
-    const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
 
-    useEffect(() => {
-       console.log('Rendering each time!', userIngredients);
-    }, [userIngredients]);
+  useEffect(() => {
+    console.log('Rendering each time!', userIngredients);
+  }, [userIngredients]);
 
-    const filteredIngredientsHandler = useCallback(filteredIngredients => {
-        setUserIngredients(filteredIngredients);
-    }, []);
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-update-47a88.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
+    });
+  };
 
-    const addIngredientHandler = ingredient => {
-        fetch('https://react-hooks-update-47a88.firebaseio.com/ingredients.json', {
-            method: 'POST',
-            body: JSON.stringify(ingredient),
-            headers: {'Content-Type': 'application/json'}
-        }).then(response => {
-            return response.json();
-        }).then(responseData => {
-            setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
-        });
-    };
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+  };
 
-    const removeIngredientHandler = ingredientId => {
-        setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
-    };
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
 
   return (
     <div className="App">
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo(props => {
-    const {onLoadIngredients} = props;
-    const [enteredFilter, setEnteredFilter] = useState('');
+  const {onLoadIngredients} = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
 
     useEffect(() => {
-        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch('https://react-hooks-update-47a88.firebaseio.com/ingredients.json' + query).then(response => response.json()).then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-                loadedIngredients.push({
-                    id: key,
-                    title: responseData[key].title,
-                    amount: responseData[key].amount
-                });
-            }
-            onLoadIngredients(loadedIngredients);
-        });
+      const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+      fetch('https://react-hooks-update-47a88.firebaseio.com/ingredients.json' + query).then(response => response.json()).then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        onLoadIngredients(loadedIngredients);
+      });
     }, [enteredFilter, onLoadIngredients]);
 
   return (
